8f8958065d1304cbf7e2c37316e3febfbf723109
fancidev <fancidev@gmail.com>
2015-12-10 16:34:44 +0800

    Refactor Vasicek calibration code.

--- WebDemo/src/Vasicek.ts
+++ WebDemo/src/Vasicek.ts
@@ -111,19 +111,92 @@ class VasicekModel implements YieldCurveModel {
 class VasicekModelTemplate implements YieldCurveModelTemplate {
 	public name: string;
 	private ts: number[];
+	private covar: number[][];
 
 	constructor(ts: number[]) {
-		this.name = ts.length + '-factor Vasicek';
+		const n = ts.length;
+		this.name = n + '-factor Vasicek';
 		this.ts = ts.slice();
+		this.covar = numeric.mul(0, numeric.identity(n));
 	}
 
-	createModel(instruments: Instrument[]): YieldCurveModel {
+	createModel(instruments: Instrument[]): VasicekModel {
 		for (let i = instruments.length - 1; i >= 0; i--) {
 			const t = instruments[i].maturity();
 			if (this.ts.indexOf(t) < 0)
 				instruments.splice(i, 1);
 		}
-		return new VasicekModel(instruments.map(maturity));
+		const model = new VasicekModel(instruments.map(maturity));
+		const covar = model.covariance();
+		for (let i = 0; i < covar.length; i++) {
+			for (let j = 0; j < covar.length; j++) {
+				covar[i][j] = this.covar[i][j];
+			}
+		}
+		return model;
+	}
+	
+	/**
+	 * Calibrates the structural parameters of Vasicek model to
+	 * a panel data of historical rates.
+	 */
+	calibrate(instruments: Instrument[], marketRates: PanelData) {
+
+		const numDates = numRows(marketRates);
+		const numSeries = numCols(marketRates);
+		if (numSeries !== instruments.length)
+			throw new RangeError('Instruments must match marketRates in size');
+
+		for (let iter = 1; iter < 50; iter++) {
+		
+			// Fit the model to historical data to get state vector time series.
+			const stateHistory: PanelData = new Array<Array<number>>();
+			const model = this.createModel(instruments);
+			for (let i = 0; i < numDates; i++) {
+				fitYieldCurve(model, instruments, marketRates[i]);
+				stateHistory[i] = model.state().slice();
+			}
+		
+			// Compute the historical covariance matrix of state vector.
+			stateHistory.rownames = marketRates.rownames;
+			const stateDiff = diff(stateHistory);
+			const covar = covariance(stateDiff);
+			numeric.muleq(covar, 250);
+		
+			// Debug print.
+			const volatility = getStdev(covar);
+			console.log('Realized factor volatility:');
+			console.log(numeric.prettyPrint(volatility));
+			const correl = getCorrelation(covar);
+			console.log('Realized factor correlation:');
+			console.log(numeric.prettyPrint(correl));
+		
+			// Compare the historical covariance with the model covariance.
+			// Because the w parameter in Vasicek model may be treated as
+			// either state variable or structural variable, we only compare
+			// as many entries as present in the model covar matrix.
+			const modelCovar = model.covariance();
+			const nf = modelCovar.length;
+			let maxDiff = -Infinity;
+			for (let i = 0; i < nf; i++) {
+				for (let j = 0; j < nf; j++) {
+					maxDiff = Math.max(maxDiff, Math.abs(covar[i][j] - modelCovar[i][j]));
+				}
+			}
+			if (maxDiff < 1e-6) { // 0.1% tolerance on volatility
+				alert('Calibration finished in ' + iter + ' iterations.');
+				return;
+			}
+	
+			// Update model covariance and then continue iteration.
+			for (let i = 0; i < nf; i++) {
+				for (let j = 0; j < nf; j++) {
+					this.covar[i][j] = covar[i][j];
+				}
+			}
+		}
+		alert('Calibration failed to converge');
+		throw new Error('Calibration failed to converge');
 	}
 }
 
