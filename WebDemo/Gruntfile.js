42867f48d8715efa57c2d4d5b75a917f02fa8cd2
fancidev <fancidev@gmail.com>
2015-12-10 12:00:52 +0800

    Use bower/grunt tool chain.

---
+++ WebDemo/Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+	
+	grunt.initConfig({
+		copy: {
+			numericjs: {
+				src: 'bower_components/numeric-1.2.6/index.js',
+				dest: 'scripts/numeric.js'
+			},
+			jquery: {
+				src: 'bower_components/jquery/dist/jquery.min.js',
+				dest: 'scripts/jquery.min.js'
+			},
+			canvasjs: {
+				files: [
+					{ src: 'src/jquery.canvasjs.js', dest: 'scripts/jquery.canvasjs.js' },
+					{ src: 'src/canvasjs-1.8.0-beta4-mod.js', dest: 'scripts/canvasjs.js' }
+				]
+			},
+			jqwidgets: {
+				cwd: 'bower_components/jqwidgets',
+				src: 'jqwidgets/*',
+				dest: 'scripts',
+				expand: true
+			}
+		},
+		clean: [ 'scripts' ]
+	});
+	
+	// Load tasks.
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	
+	// Register tasks.
+	grunt.registerTask('default', ['copy']);
+	grunt.registerTask('help', 'Display test message.', function() {
+		grunt.log.write('Hello world!\n').ok();
+	});
+	
+};
