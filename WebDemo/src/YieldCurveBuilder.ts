e5d89fc1d0cf5d72d8594b002a305e2fae2cd7a6
fancidev <fancidev@gmail.com>
2015-12-27 11:15:16 +0800

    Fix error in handling model-specific linear constraints.

--- WebDemo/src/YieldCurveBuilder.ts
+++ WebDemo/src/YieldCurveBuilder.ts
@@ -5,7 +5,7 @@ interface YieldCurveModel {
 	 * Gets a reference to the internal state vector of the model.
 	 * Changing this vector will change the model.
 	 */
-	state(): number[];
+    state(): number[];
 	
 	/**
 	 * Returns the discount factor for a given time-to-maturity.
@@ -14,7 +14,7 @@ interface YieldCurveModel {
 	 *                  respect to the internal state vector.
 	 * @returns Discount factor.  
 	 */
-	discount(t: number, gradient?: number[]): number;
+    discount(t: number, gradient?: number[]): number;
 	
 	/**
 	 * Gets model-dependent linear constraints imposed on the state vector.
@@ -29,12 +29,23 @@ interface YieldCurveModel {
 	 * If this member is not defined, it is assumed to have no particular
 	 * linear constraint. 
 	 */
-	constraints?(): [number[][], number[]];
+    constraints?(): [number[][], number[]];
+
+	/**
+	 * If there are more state variables than constraints, the Newton
+	 * solver assumes the curve should be fit by optimizing a quadratic
+	 * form z = 0.5 * s' * H * s where s is the state vector (of n
+	 * elements) and H is an n-by-n positive definite matrix returned by
+	 * this function.
+	 * 
+	 * Note that the returned matrix is not necessarily invertible.
+	 */
+    quadratic?(): number[][];
 	
 	/**
 	 * Returns an HTML segment that describes the attributes of this model.
 	 */
-	info?(): string;
+    info?(): string;
 }
 
 /**
@@ -46,7 +57,7 @@ interface YieldCurveModelTemplate {
 	/**
 	 * Gets the name of the model template.
 	 */
-	name: string;
+    name: string;
 	
 	/**
 	 * Creates a model that is ready to be fitted to market data.
@@ -56,7 +67,7 @@ interface YieldCurveModelTemplate {
 	 *                     instruments that it doesn't want to fit
 	 *                     from this array.
 	 */
-	createModel(instruments: Instrument[]): YieldCurveModel;
+    createModel(instruments: Instrument[]): YieldCurveModel;
 }
 
 /**
@@ -75,70 +86,71 @@ interface YieldCurveModelTemplate {
  */
 function fitYieldCurve(model: YieldCurveModel, instruments: Instrument[], marketRates: number[]): Discount {
 
-	const n = instruments.length;
-	if (n !== marketRates.length) {
-		throw new RangeError('instruments and marketRates must have the same length.');
-	}
+    const n = instruments.length;
+    if (n !== marketRates.length) {
+        throw new RangeError('instruments and marketRates must have the same length.');
+    }
 
-	const m = model.constraints ? model.constraints()[1].length : 0;
-	if (n + m !== model.state().length) {
-		throw new RangeError("Incorrect number of instruments.");
-	}
+    const m = model.constraints ? model.constraints()[1].length : 0;
+    if (n + m !== model.state().length) {
+        throw new RangeError("Incorrect number of instruments.");
+    }
 	
-	// Creates a closure of the discount function.
-	function discount(t: number, gradient?: number[]): number {
-		return model.discount(t, gradient);
-	}
+    // Creates a closure of the discount function.
+    function discount(t: number, gradient?: number[]): number {
+        return model.discount(t, gradient);
+    }
 	
-	// Use Newton's method to fit the model. In each iteration, we
-	// solve a linear system C*delta=marketRates-impliedRates, where
-	//
-	//   C: an (n+m)-by-(n+m) matrix, where the first m rows is the
-	//   Jacobian matrix, i.e. J[i][j] is the partial derivative of
-	//   the i'th instrument with respect to the j'th state variable;
-	//   the rest m rows are model-dependent linear constraints on
-	//   the state variables; 
-	//
-	//   delta: (n+m)-by-1 vector of adjustments to be apply to the
-	//   state vector after the iteration.
-	//
-	for (let iter = 1; iter <= 100; ++iter) {
+    // Use Newton's method to fit the model. In each iteration, we
+    // solve a linear system C*delta=marketRates-impliedRates, where
+    //
+    //   C: an (n+m)-by-(n+m) matrix, where the first n rows is the
+    //   Jacobian matrix, i.e. J[i][j] is the partial derivative of
+    //   the i'th instrument with respect to the j'th state variable;
+    //   the rest m rows are model-dependent linear constraints on
+    //   the state variables; 
+    //
+    //   delta: (n+m)-by-1 vector of adjustments to be apply to the
+    //   state vector after the iteration.
+    //
+    for (let iter = 1; iter <= 100; ++iter) {
 
-		// Compute implied rates and Jacobian.
-		const C = new Array<Array<number>>(n + m);
-		const impliedRates = new Array<number>(n);
-		for (let i = 0; i < n; i++) {
-			const gradient = new Array<number>(n + m);
-			const impliedRate = instruments[i].impliedRate(discount, gradient);
-			C[i] = gradient;
-			impliedRates[i] = impliedRate;
-		}
+        // Compute implied rates and Jacobian.
+        const C = new Array<Array<number>>(n + m);
+        const impliedRates = new Array<number>(n);
+        for (let i = 0; i < n; i++) {
+            const gradient = new Array<number>(n + m);
+            const impliedRate = instruments[i].impliedRate(discount, gradient);
+            C[i] = gradient;
+            impliedRates[i] = impliedRate;
+        }
 			
-		// Check tolerance.
-		const diff = numeric.sub(marketRates, impliedRates);
-		const maxDiff = Math.max.apply(null, diff.map(Math.abs));
-		const eps = 1.0e-8; // 0.0001 bp
-		if (maxDiff < eps) {
-			console.log('Newton method found solution in ' + iter + ' iterations.');
-			return discount;
-		}
-		const b = diff;
+        // Check tolerance.
+        const diff = numeric.sub(marketRates, impliedRates);
+        const maxDiff = Math.max.apply(null, diff.map(Math.abs));
+        const eps = 1.0e-8; // 0.0001 bp
+        if (maxDiff < eps) {
+            console.log('Newton method found solution in ' + iter + ' iterations.');
+            return discount;
+        }
+        const b = diff;
 		
-		// Add model-dependent constraints.
-		if (m > 0) {
-			const [extraConstraints, extraValues] = model.constraints();
-			for (let i = 0; i < m; i++) {
-				C[n + i] = extraConstraints[i];
-				b[n + i] = extraValues[i];
-			}
-		}
+        // Add model-dependent constraints.
+        if (m > 0) {
+            const state = model.state();
+            const [extraConstraints, extraValues] = model.constraints();
+            for (let i = 0; i < m; i++) {
+                C[n + i] = extraConstraints[i];
+                b[n + i] = extraValues[i] - numeric.dot(C[n + i], state);
+            }
+        }
 		
-		// Solve the equation.
-		const delta = numeric.solve(C, b);
-		const state = model.state();
-		numeric.addeq(state, delta);
-	}
+        // Solve the equation.
+        const delta = numeric.solve(C, b);
+        const state = model.state();
+        numeric.addeq(state, delta);
+    }
 
-	alert('Newton method cannot find solution');
-	throw new Error('Newton method cannot find solution');
+    alert('Newton method cannot find solution');
+    throw new Error('Newton method cannot find solution');
 }
